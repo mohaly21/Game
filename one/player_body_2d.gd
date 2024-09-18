9753c134af5112aaa27156982c9ac5f6133fe9fd
mohaly21 <mohaly21@kingshigh.school.nz>
2024-09-18 13:26:41 +1200

    version 1

---
+++ one/player_body_2d.gd
@@ -0,0 +1,74 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+@export var ACCELERATION: float = 15.0
+@export var JUMPS = 1
+
+enum state{IDLE, RUNNING, JUMPUP, JUMPDOWN, HURT}
+var anim_state=state.IDLE
+@onready var animator = $AnimatedSprite2D
+@onready var animation_player=$AnimationPlayer
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func update_state():
+	if anim_state==state.HURT:
+		return
+	if is_on_floor():
+		if velocity==Vector2.ZERO:
+			anim_state=state.IDLE
+		elif velocity.x !=0:
+			anim_state=state.RUNNING
+	else:
+		if velocity.y<0:
+			anim_state=state.JUMPUP
+		else:
+			anim_state=state.JUMPDOWN
+			
+		
+func update_animation(direction):
+	if direction>0:
+		animator.flip_h=false
+	elif direction<0:
+		animator.flip_h=true
+	match anim_state:
+		state.IDLE:
+			animation_player.play("idle")
+		state.RUNNING:
+			animation_player.play("run")
+		state.JUMPUP:
+			animation_player.play("jump_up")
+		state.JUMPDOWN:
+			animation_player.play("jump_down")
+		state.HURT:
+			animation_player.play("hurt")
+			
+			
+
+	
+		
+	
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = move_toward(velocity.x,SPEED,direction * ACCELERATION)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	update_state()
+	update_animation(direction)
+	move_and_slide()
