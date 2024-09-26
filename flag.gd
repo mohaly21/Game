e264e5939a22095caf981e122d78b8a3c1dc2783
mohaly21 <mohaly21@kingshigh.school.nz>
2024-09-26 13:19:56 +1200

    1.5 version

---
+++ flag.gd
@@ -0,0 +1,16 @@
+extends AnimatedSprite2D
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file("res://win.tscn")
+	else:
+		pass # Replace with function body.
